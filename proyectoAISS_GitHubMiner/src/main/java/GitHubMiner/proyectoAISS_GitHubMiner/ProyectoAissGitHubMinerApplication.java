6af287be1bb8bfc2283627eae8a724a67fe1d0dc
= <=>
2023-04-14 11:37:33 +0200

    Creación proyecto SpringBoot

---
+++ proyectoAISS_GitHubMiner/src/main/java/GitHubMiner/proyectoAISS_GitHubMiner/ProyectoAissGitHubMinerApplication.java
@@ -0,0 +1,13 @@
+package GitHubMiner.proyectoAISS_GitHubMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAissGitHubMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAissGitHubMinerApplication.class, args);
+	}
+
+}
---
+++ proyectoAISS_GitHubMiner/src/test/java/GitHubMiner/proyectoAISS_GitHubMiner/ProyectoAissGitHubMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package GitHubMiner.proyectoAISS_GitHubMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAissGitHubMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
