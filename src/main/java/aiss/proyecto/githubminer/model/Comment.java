88d84fb2ca367696c0572f932a262a7dcc4a33f1
María Carrera <marcartal1@alum.us.es>
2023-05-03 19:17:23 +0200

    Clase Comment creada

---
+++ src/main/java/aiss/proyecto/githubminer/model/Comment.java
@@ -0,0 +1,81 @@
+package aiss.proyecto.githubminer.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comment {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("body")
+    private String body;
+    @JsonProperty("created_at")
+    private String createdAt;
+    @JsonProperty("updated_at")
+    private String updatedAt;
+
+    private String issueId;
+    private User author;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(String issueId) {
+        this.issueId = issueId;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", body='" + body + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                ", updatedAt='" + updatedAt + '\'' +
+                ", issueId='" + issueId + '\'' +
+                ", author=" + author +
+                '}';
+    }
+
+}
