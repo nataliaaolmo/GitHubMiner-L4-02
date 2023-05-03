8011ece95072cfd38b47c5ae561c0dc5500cff37
María Carrera <marcartal1@alum.us.es>
2023-05-03 13:04:45 +0200

    PRUEBA - volver a montar la estructura del proyecto 2

---
+++ src/main/java/aiss/proyecto/githubminer/GithubminerApplication.java
@@ -0,0 +1,13 @@
+package aiss.proyecto.githubminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubminerApplication.class, args);
+	}
+
+}
